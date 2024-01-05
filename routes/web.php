76bdc87e20883e54e81744fa971c2752084d393f
mahsanriaz-github <ahsanrazawebmaster@gmail.com>
2024-01-04 16:36:05 -0800

    change route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\FormBuilderController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can it register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
